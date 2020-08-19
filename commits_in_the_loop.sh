d423bf507689af118a2387b8bf8fa70ec325b641
keska <artur.keska@tomtom.com>
2020-08-19 20:51:55 +0200

    skip some test, too much granularity

--- commits_in_the_loop.sh
+++ commits_in_the_loop.sh
@@ -1,23 +1,21 @@
 #!/usr/bin/env bash
 
-for p in {1..100} ; do
-    for d in {1..100} ; do
-        for r in {1..100} ; do
-            export PARENTS=${p}
-            export DEPENDANTS=${d}
-            export REPEAT=${r}
-            echo "[INFO]: STARTING parents ${p} dependants ${d} repeats ${r}"
+for p in {1..22} ; do
+    for d in {1..22} ; do
+            export PARENTS=$((p*5-4))
+            export DEPENDANTS=$((d*5-4))
+            export REPEAT=5
+            echo "[INFO]: STARTING parents ${PARENTS} dependants ${DEPENDANTS} repeats ${REPEAT}"
             beginTime=$(($(date +%s%N)/1000))
-            LOGFILE="test_parents_${p}_dependants${d}_repeat_${r}.log"
-            echo "[INFO]: START,${p},${d},${r},${beginTime}" > runlog.log
+            LOGFILE="test_parents_${PARENTS}_dependants${DEPENDANTS}_repeat_${REPEAT}.log"
+            echo "[INFO]: START,${PARENTS},${DEPENDANTS},${REPEAT},${beginTime}" > runlog.log
             mvn test -Dtest=com.tomtom.aktools.CommitingTest#massiveCommitInOrderTest > ${LOGFILE} 2>&1
             endTime=$(($(date +%s%N)/1000))
             echo "[INFO]: $(grep AVG_COMMIT_TIME_MS ${LOGFILE} | less)"
             echo "[INFO]: $(grep TOTALL_COMMIT_TIME_MS ${LOGFILE} | less)"
-            echo "[INFO]: END,${p},${d},${r},${endTime}, $((endTime-beginTime)), ERRORS: $(grep ERROR ${LOGFILE} | wc)"
-            echo "[INFO]: END,${p},${d},${r},${endTime}, $((endTime-beginTime)), ERRORS: $(grep ERROR ${LOGFILE} | wc)"
-            echo "[INFO]: END,${p},${d},${r},${endTime}, $((endTime-beginTime))"  > runlog.log
-        done
+            errors=$(grep ERROR ${LOGFILE} | wc | awk '{print $2}')
+            echo "[INFO]: END,${PARENTS},${DEPENDANTS},${REPEAT},${endTime}, $((endTime-beginTime)), ${errors}"
+            echo "[INFO]: END,${PARENTS},${DEPENDANTS},${REPEAT},${endTime}, $((endTime-beginTime)), ${errors}" > runlog.log
     done
 done
 
